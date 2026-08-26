352372020ad556175852cb275f7d4be3d02fc460
Fantom <fantom.weblabs@gmail.com>
2026-08-26 16:40:29 -0300

    Estrutura incial do repo

--- Estrutura de Dados em Python/LISTA DE EXERCÍCIO – 03 – Revisão em Python/1.py
+++ Estrutura de Dados em Python/LISTA DE EXERCÍCIO – 03 – Revisão em Python/1.py
@@ -3,5 +3,20 @@ Escreva um programa que recebe cinco notas de um aluno e calcula a média. Em se
 aluno foi aprovado (média maior ou igual a 7) ou reprovado (média menor que 7).
 
 '''
+notas = []
+
+print("Calcule sua média inserindo as 5 notas obtidas: ")
+
+for i in range(5):
+    nota = float(input("Digite a nota: "))
+    notas.append(nota)
+
+total = sum(notas)
+media = total / 5
+
+if media >= 7:
+    print("Aprovado com média" ,media)
+elif media < 7:
+    print("Reprovado com média" ,media)
 
 
